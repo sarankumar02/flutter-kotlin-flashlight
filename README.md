a588c9f7959ff47fbeb02837a4d4a3e8e7b6979c
Sarankumar02 <sarandhanush123@gmail.com>
2022-04-04 21:04:14 +0530

    assets and readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # kotlin_flashlight
 
-A new Flutter project.
+A Sample Flutter project to show how to integrate native kotlin code with flutter.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+Visit this docs for Flutter setup on your platform.
 
 A few resources to get you started if this is your first Flutter project:
 
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## To run
+
+1. Go to the project directory.
+2. Open a terminal and run the command 'flutter run'.
+3. Make sure you are running this project with a physical device.
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,21 +14,52 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     screenSize = MediaQuery.of(context).size;
     return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: const Text("Flashlight kotlin",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                ))),
         body: Container(
-      width: screenSize.width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              onPressed: () async {
-                torchOn = await _repository.turnOnOrOffFlashLight();
-                print("TORCH $torchOn");
-                setState(() {});
-              },
-              child: Text(torchOn ? 'Torch on' : 'Torch off'))
-        ],
-      ),
-    ));
+          padding: const EdgeInsets.all(20),
+          width: screenSize.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: screenSize.width * 0.8,
+                height: screenSize.height * 0.5,
+                margin: const EdgeInsets.only(bottom: 10),
+                color: Colors.red,
+                child: Image.asset(
+                  torchOn
+                      ? 'assets/images/torch_on.png'
+                      : 'assets/images/torch_off.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    torchOn = await _repository.turnOnOrOffFlashLight();
+                    setState(() {});
+                  },
+                  style: ElevatedButton.styleFrom(
+                      primary: torchOn ? Colors.white : Colors.black,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                      )),
+                  child: Text(
+                    torchOn ? 'Torch on' : 'Torch off',
+                    style:
+                        TextStyle(color: torchOn ? Colors.black : Colors.white),
+                  ))
+            ],
+          ),
+        ));
   }
 }
